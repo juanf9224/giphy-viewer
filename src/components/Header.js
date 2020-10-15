9c88572b679218fb4dc3ffae141c906bded009ab
juanf9224 <juanfh.24.6@gmail.com>
2020-10-14 22:42:33 -0400

    add app header with name

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { makeStyles, createStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => createStyles({
+  header__title: {
+    color: '#f7f7f7',
+    fontWeight: 'bold',
+    fontSize: '1.5rem'
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <header className="header">
+      <div className={classes.header__title}>
+        <h1>Gifs Viewer</h1>
+      </div>
+    </header>
+  );
+};
+
+export default (Header);
